23a85c1331812f44b4bbdbd98c494e5970a74d2f
Adam Titus <adamtitus76@gmail.com>
2018-01-12 12:57:12 -0800

    Start product section.

---
+++ GummiBear/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBear.Models;
+
+namespace GummiBear.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepo;
+        public ProductController(IProductRepository repo = null)
+        {
+            if(repo == null)
+            {
+                this.productRepo = new EFProductRepository();
+            }
+            else
+            {
+                this.productRepo = repo;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(productRepo.Products.ToList());
+        }
+    }
+}
--- GummiBear/Models/Repositories/IReviewRepository.cs
+++ GummiBear/Models/Repositories/IReviewRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GummiBear.Models.Repositories
+namespace GummiBear.Models
 {
     public interface IReviewRepository
     {
